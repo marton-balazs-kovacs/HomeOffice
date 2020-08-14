1858baf3eb1823a48c1372d2fc84209d0ffd6c5d
Marton Kovacs <marton.balazs.kovacs@gmail.com>
2020-08-14 19:59:20 +0200

    cleaned up analysis and created figures

--- R/subgroup_count.R
+++ R/subgroup_count.R
@@ -4,7 +4,7 @@ subgroup_count <- function(subgroub_name) {
     dplyr::ungroup() %>% 
     dplyr::mutate(N = sum(n),
                   prop = round(n / N * 100, 2)) %>% 
-    dplyr::arrange(n) %>% 
+    dplyr::arrange(dplyr::desc(n)) %>% 
     dplyr::rename(levels = .data[[subgroub_name]]) %>% 
     dplyr::mutate(levels = as.character(levels))
 }
